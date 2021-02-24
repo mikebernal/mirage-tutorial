c3b0a797f46879ec859117d527ab07d331a8fc78
mikebernal <mikebernal.me@gmail.com>
2021-02-24 08:22:22 +0800

    relationships

--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
 // Welcome to the tutorial!
-import { createServer, Model } from 'miragejs'
+import { createServer, Model, hasMany, belongsTo } from 'miragejs'
 
 export default function () {
     createServer({
         models: {
-            reminder: Model,
+            list: Model.extend({
+                reminders: hasMany(),
+            }),
+            reminder: Model.extend({
+                list: belongsTo(),
+            }),
         },
         seeds(server) {
-            server.create("reminder", { text: "Walk the dog" })
+            let homeList = server.create("list", { name: "Home" })
+            let workList = server.create("list", { name: "Work" })
+
+            server.create("reminder", { text: "Walk the dog", })
             server.create("reminder", { text: "Take out the trash" })
             server.create("reminder", { text: "Work out" })
+
+            server.create("reminder", { list: homeList, text: "Do taxes" })
+            server.create("reminder", { list: workList, text: "Visit bank" })
         },
         routes() {
             this.get("/api/reminders", (schema) => {
@@ -18,6 +29,7 @@ export default function () {
 
             this.post("/api/reminders", (schema, request) => {
                 let attrs = JSON.parse(request.requestBody)
+                console.log(attrs)
                 return schema.reminders.create(attrs)
             })
 
@@ -25,6 +37,17 @@ export default function () {
                 let id = request.params.id
 
                 return schema.reminders.find(id).destroy()
+            })
+
+            this.get('/api/lists', (schema, request) => {
+                return schema.lists.all()
+            })
+
+            this.get('/api/lists/:id/reminders', (schema, request) => {
+                let listId = request.params.id
+                let list = schema.lists.find(listId)
+
+                return list.reminders
             })
         },
     })
